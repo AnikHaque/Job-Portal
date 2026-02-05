8e92500703cab772c1e4b8acb890e1e7e9c15521
AnikHaque <ekramul15-811@diu.edu.bd>
2026-02-06 02:15:47 +0600

    Fix ALLOWED_HOSTS and CSRF for Render

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'https://job-portal-cedv.onrender.com',   # 🔁 এখানে তোমার Render app name
+    'job-portal-cedv.onrender.com',   # 🔁 এখানে তোমার Render app name
 ]
 
 
